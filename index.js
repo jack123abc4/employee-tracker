b3bc2bd5a4a7bf2792b6c1adaea4e887f3995495
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-19 22:29:32 -0400

    custom 'none' text on employees without managers

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ function printTable(sql)  {
     });
 }
 
-
 let printDepartments = printTable(
     `SELECT d.id AS DEPARTMENT_ID, d.department_name AS NAME
     FROM departments AS d`
@@ -37,17 +36,23 @@ let printRoles = printTable(
     JOIN departments AS d ON r.department_id = d.id`
 );
 let printEmployees = printTable(
-    `SELECT e.id AS EMPLOYEE_ID, e.first_name AS FIRST_NAME, e.last_name AS LAST_NAME, r.title as TITLE, d.department_name AS DEPARTMENT_NAME, r.salary AS SALARY, CONCAT(m.first_name, ' ', m.last_name) AS MANAGER
+    `SELECT e.id AS EMPLOYEE_ID, e.first_name AS FIRST_NAME, e.last_name AS LAST_NAME, r.title as TITLE, d.department_name AS DEPARTMENT_NAME, r.salary AS SALARY, IFNULL(CONCAT(m.first_name, ' ', m.last_name), "none") AS MANAGER
     FROM employees AS e
     JOIN roles AS r ON e.role_id = r.id
     JOIN departments AS d ON r.department_id = d.id
     LEFT JOIN employees AS m ON e.manager_id = m.id`
 );
 
+function getNextTask() {
+
+}
+
 function init() {
     printDepartments; // view all departments
     printRoles; // view all roles
     printEmployees; // view all employees
+
+    getNextTask();
 }
 
 init();
