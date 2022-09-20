0112650f01ab7cc074f5bf298a04b02102fa7e2c
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-19 22:19:28 -0400

    employee table join in mysql shell

--- db/queries.sql
+++ db/queries.sql
@@ -22,4 +22,14 @@ FROM departments AS d;
 -- job title, role id, department that role belongs to, salary for that role
 SELECT r.id AS ROLE_ID, r.title AS TITLE, r.salary AS SALARY, d.department_name AS DEPARTMENT_NAME
 FROM roles AS r
-LEFT JOIN departments AS d ON r.department_id = d.id;
+JOIN departments AS d ON r.department_id = d.id;
+
+
+-- EMPLOYEES PRINT OUT
+-- employee id, first name, last name, job title, department, salary, manager
+SELECT e.id AS EMPLOYEE_ID, e.first_name AS FIRST_NAME, e.last_name AS LAST_NAME, r.title as TITLE, d.department_name AS DEPARTMENT_NAME, r.salary AS SALARY, CONCAT(m.first_name, ' ', m.last_name) AS MANAGER
+FROM employees AS e
+JOIN roles AS r ON e.role_id = r.id
+JOIN departments AS d ON r.department_id = d.id
+INNER JOIN employees AS m ON e.manager_id = m.id;
+
