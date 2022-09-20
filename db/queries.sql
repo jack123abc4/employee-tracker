c6cd9db1195153a3e061495f9dbdec249e0c31db
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-19 21:32:17 -0400

    print departments + roles

--- db/queries.sql
+++ db/queries.sql
@@ -1,11 +1,25 @@
-SELECT * FROM departments;
-SELECT * FROM roles;
-SELECT * FROM employees;
+-- SELECT * FROM departments;
+-- SELECT * FROM roles;
+-- SELECT * FROM employees;
 
 -- SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary
 -- FROM employees
 -- LEFT JOIN roles ON employees.role_id = roles.id;
 
-SELECT e.id AS EMPLOYEE_ID, e.first_name AS FIRST_NAME, e.last_name AS LAST_NAME, r.title AS TITLE, r.salary AS SALARY
-FROM employees AS e
-LEFT JOIN roles AS r ON e.role_id = r.id;
+-- SELECT e.id AS EMPLOYEE_ID, e.first_name AS FIRST_NAME, e.last_name AS LAST_NAME, r.title AS TITLE, r.salary AS SALARY
+-- FROM employees AS e
+-- LEFT JOIN roles AS r ON e.role_id = r.id;
+
+
+
+
+
+-- DEPARTMENT PRINT OUT
+SELECT d.id AS DEPARTMENT_ID, d.department_name AS NAME
+FROM departments AS d;
+
+-- ROLES PRINT OUT
+-- job title, role id, department that role belongs to, salary for that role
+SELECT r.id AS ROLE_ID, r.title AS TITLE, r.salary AS SALARY, d.department_name AS DEPARTMENT_NAME
+FROM roles AS r
+LEFT JOIN departments AS d ON r.department_id = d.id;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const { inherits } = require('util');
+const { resolve } = require('path');
 
 const db = mysql.createConnection(
     {
@@ -14,6 +15,33 @@ const db = mysql.createConnection(
     console.log('Connected to the company_db database')
 );
 
-function init() {
-    
+function printTable(sql)  {
+    db.query(sql, (err, result) => {
+        if (err) {
+          result.status(400).json({ error: err.message });
+          return;
+        }
+        console.table(result);
+        return result;
+    });
 }
+
+
+let printDepartments = printTable(
+    `SELECT d.id AS DEPARTMENT_ID, d.department_name AS NAME
+    FROM departments AS d;`
+    );
+let printRoles = printTable(
+    `SELECT r.id AS ROLE_ID, r.title AS TITLE, r.salary AS SALARY, d.department_name AS DEPARTMENT_NAME
+FROM roles AS r
+LEFT JOIN departments AS d ON r.department_id = d.id;`
+)
+
+
+function init() {
+    printDepartments; // view all departments
+    printRoles; // view all roles
+    //printTable("employees"); // view all employees
+}
+
+init();
